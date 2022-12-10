d8d5c1f3f6707c844444864a5ca15a1414cf121b
TeHb19 <53332538+TeHb19@users.noreply.github.com>
2022-12-10 17:04:58 +0600

    Add files via upload
    минус баги, плюс графика

--- streetlight.py
+++ streetlight.py
@@ -1,68 +1,98 @@
 from tkinter import *
 import time
 
-'''
-на потом
 
-WIDTH=500
-HEIGHT=500
+WIDTH=1920
+HEIGHT=1080
+ptom=50
 
 window=Tk()
 
 canvas=Canvas(window,width=WIDTH,height=HEIGHT)
-#canvas.pack()
-'''
-
+canvas.pack()
 
 #считывает машины, сначала координату, потом скорость
 # pos speed
 
-
 carsx1=[]
 carsy1=[]
+carblue=PhotoImage(file='bruh1.png')
+carred=PhotoImage(file='car1.png')
+
 with open('carsx1.txt','r') as file:
     for i in file.readlines():
-        carsx1.append(list(map(int,i.split())))
+        a=list(map(int,i.split()))
+        a.append(canvas.create_image(-a[0]*ptom,400,image=carblue,anchor=NW))
+        carsx1.append(a)
+        
+
 with open('carsy1.txt','r') as file:
-    for i in file.readline():
-        carsy1.append(list(map(int,i.split())))
+    for i in file.readlines():
+        a=list(map(int,i.split()))
+        a.append(canvas.create_image(900,500-a[0]*ptom,image=carred))
+        carsy1.append(a)
+        
+
 
 #переменные, мин промежуток времени, макс скорость, ускорение/замедление
-dt=1/1000
+dt=1/24
 max_speed=50/3
 dv=max_speed/7
+cycle=60
 
 
 #как ведут себя машины на жёлтом/красном
-def stop(cars):
+def stop(cars,dir):
     for i in range(len(cars)):
             #едет вперед со своей скоростью
+            if dir:
+                canvas.move(cars[i][-1],cars[i][1]*ptom*dt,0)
+            else:
+                canvas.move(cars[i][-1],0,cars[i][1]*ptom*dt)
             cars[i][0]-=cars[i][1]*dt
 
             #смотрит расстояние до светофора/ближайшей машины спереди
             if i:
-                a=min(cars[i][0]-cars[i-1][0],cars[i][0])
+                #если близко и едет, то замедляемя
+                if 0<cars[i][0]-cars[i-1][0]<60 and cars[i][1]>0 and cars[i-1][1]<max_speed:
+                  cars[i][1]-=dt*dv
+                  if cars[i][1]<0:
+                    cars[i][1]=0
             
+                #иначе если не на макс скорости, то ускоряемся
+                elif 0<cars[i][1]<max_speed:
+                    cars[i][1]+=dt*dv
+                    if cars[i][1]>max_speed:
+                        cars[i][1]=max_speed
             else:
-                a=cars[0][0]
+                #если близко и едет, то замедляемя
+                if 0<cars[i][0]<60 and cars[i][1]>0:
+                  cars[i][1]-=dt*dv
+                  if cars[i][1]<0:
+                    cars[i][1]=0
             
-            #если близко и едет, то замедляемя
-            if 0<a<60 and cars[i][1]>0:
-                cars[i][1]-=dt*dv
-            
-            #иначе если не на макс скорости, то ускоряемся
-            elif cars[i][0]<max_speed:
-                cars[i][1]+=dt*dv
+                #иначе если не на макс скорости, то ускоряемся
+                elif 0<cars[i][1]<max_speed:
+                    cars[i][1]+=dt*dv
+                    if cars[i][1]>max_speed:
+                        cars[i][1]=max_speed
+
 
 #как машины ведут себя на зеленом
-def go(cars):
+def go(cars,dir):
     for car in cars:
+            if dir:
+                canvas.move(car[-1],car[1]*ptom*dt,0)
+            else:
+                canvas.move(car[-1],0,car[1]*ptom*dt)
             car[0]-=car[1]*dt
             if car[1]<max_speed:
                 car[1]+=dt*dv
+                if car[1]>max_speed:
+                    car[1]=max_speed
 
 
-for cycle in range(10):
+for cyc in range(10):
 
     incoming_x=0
     incoming_y=0
@@ -76,23 +106,38 @@ for cycle in range(10):
         if car[0]<100:
             incoming_y+=1
 
+    print(incoming_x,incoming_y)
     #распределяем время соответсвено
     if incoming_x==incoming_y:
-        green=60/dt
+        green=int(cycle/(2*dt))
     else:
         weight=incoming_x/(incoming_x+incoming_y)
-        green=int(120*incoming_x/(dt*weight))
+        print(cycle,weight,dt,cycle*weight/dt)
+        green=int(cycle*weight/dt)
 
     #говорит что должны делать машины на зеленый,желтый,красный,желтый соответсвено
+    print('green',green)
     for i in range(green):
-        go(carsx1)
-        stop(carsy1)
-    for i in range(7/dt):
-        stop(carsx1)
-        stop(carsy1)
-    for i in range(120/dt-green):
-        stop(carsx1)
-        go(carsy1)
-    for i in range(7/dt):
-        stop(carsx1)
-        stop(carsy1)
+        go(carsx1,1)
+        stop(carsy1,0)
+        window.update()
+        time.sleep(dt)
+    print('yellow')
+    for i in range(int(7/dt)):
+        stop(carsx1,1)
+        stop(carsy1,0)
+        window.update()
+        time.sleep(dt)
+    print('red',int(cycle/dt)-green)
+    for i in range(int(cycle/dt)-green):
+        stop(carsx1,1)
+        go(carsy1,0)
+        window.update()
+        time.sleep(dt)
+    print('yellow')
+    for i in range(int(7/dt)):
+        stop(carsx1,1)
+        stop(carsy1,0)
+        window.update()
+        time.sleep(dt)
+        
